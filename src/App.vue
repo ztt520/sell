139f286a1f8d59a824ce4c4d14f257b66eedb2b7
邹婷婷 <437482411@qq.com>
2018-06-12 18:09:09 +0800

    弹层页面

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header>
     <div class="tab">
       <div class="tab-item">
         <!--商品-->
@@ -26,42 +26,49 @@
 </template>
 
 <script>
-  import header from "./components/header/header.vue";
-
-  export default {
-    name: 'App',
-    data(){
-      return {
-        seller:{}
-      };
-    },
-    components: {
-      'v-header': header
-    }
+import header from "./components/header/header.vue";
+const ERR_OK = 0;
+export default {
+  name: "App",
+  data() {
+    return {
+      seller: {}
+    };
+  },
+  created() {
+    this.$http.get("api/seller").then((response) => {
+      response=response.body;
+      if(response.errno ===ERR_OK){
+        this.seller=response.data;
+        console.log(this.seller)
+      }
+    });
+  },
+  components: {
+    "v-header": header
   }
+};
 </script>
 
 <style scoped>
-  .tab {
-    display: flex;
-    width: 100%;
-    height: 40px;
-    line-height: 40px;
-    border-bottom: 1px solid rgba(7, 17, 27, 0.1);
-  }
+.tab {
+  display: flex;
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+}
 
-  .tab-item {
-    flex: 1;
-    text-align: center;
-  }
-  .tab-item a{
-    display: block;
-    font-size: 14px;
-    color:rgb(77,85,93);
-  }
-  .tab-item a.active{
-    color:rgb(240,20,20);
-  }
-
-
+.tab-item {
+  flex: 1;
+  text-align: center;
+}
+.tab-item a {
+  display: block;
+  font-size: 14px;
+  color: rgb(77, 85, 93);
+}
+.tab-item a.active {
+  color: rgb(240, 20, 20);
+}
 </style>
